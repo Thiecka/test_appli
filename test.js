df36584c0a48171e090fd136798e28de77ff890f
Die Aissatou Dieng <dieaissatou.dieng@segula.fr>
2023-06-13 12:05:28 +0200

    premier commit

---
+++ test.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const port = 8080;
+
+app.get('/', (req, res) => {
+  res.send('Hello Word!')
+});
+
+app.listen(port, () => {
+  console.log(`Application exemple est à l'écoute sur le port ${port}!`)
+});
