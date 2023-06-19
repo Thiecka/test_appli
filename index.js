ae8b200c799b8800dc34bb730a377ee4bd4df644
Die Aissatou Dieng <dieaissatou.dieng@segula.fr>
2023-06-19 13:31:38 +0200

    build 1

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 const express = require('express');
 const app = express();
-const port = 8080;
+const port = process.env.PORT || 8080;
+//const port = 8080;
 
 const http = require('stream-http');
 const stream = require('stream-browserify');
 const url = require('url');
 const util = require('util');
+
+const env = process.env.NODE_ENV;
+console.log('Environment:', env);
+
+// You can also set a default value if the environment variable is not defined
 
 
 
